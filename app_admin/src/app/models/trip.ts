a6601457313ba0beff0533c294c9808961c3aab3
Malcolm Loveless <mloveless1@users.noreply.github.com>
2023-08-06 21:35:55 -0700

    Created SPA

---
+++ app_admin/src/app/models/trip.ts
@@ -0,0 +1,11 @@
+export interface Trip {
+    _id: string,
+    code: string,
+    name: string,
+    length: string,
+    start: Date,
+    restor: string,
+    perPerson: string,
+    image: string,
+    description: string
+}
